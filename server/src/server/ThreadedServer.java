b333fed06c0a40d0b3e5e30a0801246b039f945e
MariusMM2 <dmunteanu91@yahoo.com>
2018-10-14 13:21:09 +0200

    extracted ClientThread to own .java

--- server/src/server/ThreadedServer.java
+++ server/src/server/ThreadedServer.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class ThreadedServer {
     private static final int PORT = 2222;
     private static final int MAX_CLIENTS = 10;
-    private final class Protocols{
+    final class Protocols{
         static final String EXIT = "/exit";
 
     }
@@ -52,76 +52,4 @@ public class ThreadedServer {
         }
     }
 
-    private static class ClientThread extends Thread {
-        private BufferedReader is;
-        private PrintStream os;
-        private Socket clientSocket;
-        private final ClientThread[] threads;
-        private int maxClientsCount;
-
-        ClientThread(Socket clientSocket, ClientThread[] threads) {
-            this.clientSocket = clientSocket;
-            this.threads = threads;
-            maxClientsCount = threads.length;
-        }
-
-        @Override
-        public void run() {
-            try {
-                is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                os = new PrintStream(clientSocket.getOutputStream());
-                System.out.println("Asking for name");
-                os.println("Enter your name: ");
-                String name = is.readLine().trim();
-                os.println("Hello, " + name);
-                synchronized (this) {
-                    for (int i = 0; i < maxClientsCount; i++) {
-                        if (threads[i] != null && threads[i] != this) {
-                            threads[i].os.println("User " + name + " entered the chat room.");
-                        }
-                    }
-                }
-                synchronized (this) {
-                    while (true) {
-                        String line = is.readLine();
-                        if (line.startsWith(Protocols.EXIT)) {
-                            break;
-                        }
-                        for (int i = 0; i < maxClientsCount; i++) {
-                            if (threads[i] != null) {
-                                threads[i].os.println("<" + name + "> " + line);
-                            }
-                        }
-                    }
-                }
-                for (int i = 0; i < maxClientsCount; i++) {
-                    if (threads[i] != null && threads[i] != this) {
-                        threads[i].os.println("User " + name + " left the chat room.");
-                    }
-                }
-
-                os.println("Bye, " + name + "!");
-
-                synchronized (this) {
-                    for (int i = 0; i < maxClientsCount; i++) {
-                        if (threads[i] == this) {
-                            threads[i] = null;
-                        }
-                    }
-                }
-
-                is.close();
-                os.close();
-                clientSocket.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "ClientThread";
-        }
-    }
 }
