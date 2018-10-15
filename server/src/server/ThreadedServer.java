2093531bab56709c56fd9ce95829a4674f6e1bc9
MariusMM2 <dmunteanu91@yahoo.com>
2018-10-15 10:24:49 +0200

    comments, small code cleanup

--- server/src/server/ThreadedServer.java
+++ server/src/server/ThreadedServer.java
@@ -3,22 +3,28 @@ package server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
+/**
+ * The entry point for the server, this class is responsible for
+ * keeping references to every client connected to the server,
+ * as well as configuring new clients that join the chat room
+ */
 public class ThreadedServer {
+    //The port number
     private static final int PORT = 2222;
+    //Max number of clients connected at one time
     private static final int MAX_CLIENTS = 10;
+    //Array of clients; +1 to hold the server's own client thread
+    private static final ClientThread[] threads = new ClientThread[MAX_CLIENTS+1];
+    //The server socket
+    private static ServerSocket serverSocket;
+    //Container class for protocol types and response messages
     final class Protocols{
-        static final String EXIT = "exit";
-        static final String MSG_INVALID = "Invalid command";
-
+        static final String EXIT = "exit",
+                            MSG_INVALID = "Invalid command";
     }
 
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-
-    private static final ClientThread[] threads = new ClientThread[MAX_CLIENTS];
-
+    //main Server thread
     public static void main(String[] args) {
         try {
             System.out.println("Creating server socket");
@@ -29,10 +35,12 @@ public class ThreadedServer {
 
         while (true){
             try {
-                System.out.println("Waiting for a socket");
-                clientSocket = serverSocket.accept();
-                System.out.println("accepted a socket");
-                int i = 0;
+                System.out.println("Waiting for a client");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("found a client");
+                //loops through the threads array, adding the new client
+                //to the first empty element found
+                int i;
                 for (i = 0; i < MAX_CLIENTS; i++) {
                     if (threads[i] == null){
                         threads[i] = new ClientThread(clientSocket, threads);
@@ -40,7 +48,9 @@ public class ThreadedServer {
                         break;
                     }
                 }
-                System.out.println(Arrays.toString(threads));
+                //if the loop went through the whole array without
+                //finding an empty element, this means the array is full of clients,
+                //so refuse the new client
                 if (i == MAX_CLIENTS){
                     PrintStream os = new PrintStream(clientSocket.getOutputStream());
                     os.println("Server full, try later.");
